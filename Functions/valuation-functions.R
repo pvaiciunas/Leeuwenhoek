d57c1046ad325a48b906dd88f1f182484a41c2e1
pvai <pvai@umich.edu>
2024-02-25 22:18:26 -0500

    Adding damodaran data for industry averages

--- Functions/valuation-functions.R
+++ Functions/valuation-functions.R
@@ -5,10 +5,12 @@ ConvertOpLeases <- function() {}
 
 NormalizeOpInc <- function() {
   # Need to find industry average
-  
+  # Currently taking from Damodaran, but this needs data cleaning for 
+  # industry names etc
+  url <- "https://pages.stern.nyu.edu/~adamodar/pc/fcffginzu.xlsx"
+  download.file(url, method = "curl", dest= "Data/fcffginzu.xlsx")
+  ind_avg <- read_excel("Data/fcffginzu.xlsx", sheet= "US Industry averages")  
   
 }
 
-url <- "https://pages.stern.nyu.edu/~adamodar/pc/fcffginzu.xlsx"
-download.file(url, method = "curl", dest= "Data/fcffginzu.xlsx")
-ind_avg <- read_excel("Data/fcffginzu.xlsx", sheet= "US Industry averages")
+
